41469a82bea3e4bd083d8e20f000f72ace0f2eaf
Miriam Derenbach <miriam.derenbach@outlook.com>
2021-01-09 11:40:57 +0900

    routing updated and started action

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   # > Destroy
   get '/tasks', to: 'tasks#index'
 
-  get 'tasks/new', to: 'tasks#new', as: new_restaurant
+  get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
 
   get 'tasks/:id', to: 'tasks#show'
 
-  get 'tasks/:id/edit', to: 'tasks#edit', as: edit_restaurant
+  get 'tasks/:id/edit', to: 'tasks#edit'
   patch 'tasks/:id', to: 'tasks#update'
 
   delete 'tasks/:id', to: 'tasks#destroy'
